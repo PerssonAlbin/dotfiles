a98a8db2592e93763c39861296d9af69a4d45175
PerssonAlbin <albin.persson@protonmail.com>
2023-08-08 22:56:01 +0200

    Neovim config files

---
+++ nvim/plugins.lua
@@ -0,0 +1,33 @@
+local plugins = {
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        event = "VeryLazy",
+        opts = function()
+            return require "custom.configs.null-ls"
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "clang-format",
+                "lua-language-server",
+                "autopep8",
+                "cmakelang",
+            },
+        },
+    }
+}
+return plugins
