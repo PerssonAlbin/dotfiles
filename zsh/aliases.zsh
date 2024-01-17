97381a6a2bff7b60e1b7cbc766393e20d79fa68e
Albin Persson - 2N <albinper@axis.com>
2024-01-17 13:46:15 +0100

    Aliases for git and better copy mode
    Change-Id: I41d0779557c2ec355ae01d6cc09bb1e655e12847

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,9 @@ alias ls='ls --color=auto'
 # NeoVim as Vim
 alias vim=nvim
 
+# Git
+alias gis='git status'
+alias gia='git add '
+alias gica='git commit --amend'
+alias gid='git diff '
+alias gico='git checkout '
