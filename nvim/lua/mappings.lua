01f316e9ca62a837f66ea0cb26a5442ac793c133
Albin Persson - 2N <albinper@axis.com>
2024-05-15 13:23:57 +0200

    Get original search back to default binding
    Change-Id: Icfb6cf19413d3bf9075b74e0946cba570525456f

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -20,7 +20,7 @@ map("n", "<C-q>", ":bp<bar>sp<bar>bn<bar>bd<CR>", default_options)
 
 -- Telescope
 map("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, desc = "Telescope: find files" })
-map("n", "/", ":Telescope live_grep<CR>", { noremap = true, desc = "Telescope: find string" })
+map("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true, desc = "Telescope: find string" })
 map("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true, desc = "Telescope: find open buffer" })
 map("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true })
 map("n", "<leader>fc", ":Telescope grep_string<cr>", { noremap = true, desc = "Telescope: find string under cursor" })
